c37d07f3e598087129521dfc4e7a4ddef5391990
Behnam Banaei <behban20@gmail.com>
2022-04-02 17:23:02 +0430

    initial commit

---
+++ app/src/main/java/ir/mobdevclub/marvelz/MainActivity.kt
@@ -0,0 +1,11 @@
+package ir.mobdevclub.marvelz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
